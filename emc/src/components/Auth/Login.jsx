671cdb148014fbacfab2e0d54a6cad2029b93193
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-07 17:50:33 +0530

    new

--- emc/src/components/Auth/Login.jsx
+++ emc/src/components/Auth/Login.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Login = () => {
   return (
     
-    <div className='flex items-center justify-center'> 
-        <div className='borde-2 border-blue-500'>
+    <div className='flex  h-screen w-screen items-center justify-center'> 
+        <div className='border-2 border-emerald-500 p-20'>
             <form className='flex flex-col items-center justify-center'>
-                <input type="email" placeholder='Enter Your Email' />
-                <input type="password" placeholder='Enter Password' />
+                <input required className='outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter Your Email' />
+                <input required className=' mt-3 outline-none bg-transparent border-2 border-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-gray-400' type="password" placeholder='Enter Password' />
+                <button className='mt-7 text-white outline-none border-none  hover:bg-emerald-800  bg-emerald-600 text-xl py-3 px-5 rounded-full placeholder:text-white'>Log in </button>
             </form>
+
         </div>
-        <div className='rounded border-spacing-2'>
-            <button className='rounded border-spacing-2'>Login</button>
-        </div>
+        
 
     </div>
     
