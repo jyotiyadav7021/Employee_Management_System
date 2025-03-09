e8fc7f7d2741d3658d7a531c21e923d4ed688898
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-09 19:57:36 +0530

    new

--- emc/src/components/Dashboard/AdminDashboard.jsx
+++ emc/src/components/Dashboard/AdminDashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import EmployeeDashboard from './EmployeeDashboard'
 
 const AdminDashboard = () => {
   return (
     <div>
-      
+      <EmployeeDashboard/>
     </div>
   )
 }
--- emc/src/components/Dashboard/EmployeeDashboard.jsx
+++ emc/src/components/Dashboard/EmployeeDashboard.jsx
@@ -5,6 +5,7 @@ const EmployeeDashboard = () => {
   return (
     <div>
       <Header/>
+      <EmployeeDashboard/>
     </div>
   )
 }
