263764687003c6053bfb178829fcc18b3b1d81e0
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-12 14:17:55 +0530

    new

--- emc/src/components/Dashboard/EmployeeDashboard.jsx
+++ emc/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../Other/Header'
+import TaskListNumber from '../Other/TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
   return (
-    <div>
+    <div className='p-10 bg-[#1c1c1c] h-screen'>
       <Header/>
+      <TaskListNumber/>
+      <TaskList/>
       
     </div>
   )
--- emc/src/components/Other/TaskListNumber.jsx
+++ emc/src/components/Other/TaskListNumber.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const TaskListNumber = () => {
   return (
-    <div>
+    <div className= 'flex mt-10 justify-between gap-5  screen'>
+      <div className='rounded-xl w-[45%]  bg-red-400 py-6 px-9'>
+      <h2 className='text-3xl font-semibold'>0</h2>
+      <h3 className='text-xl font-medium'>New Task</h3>
+      
+      </div>
+      <div className='rounded-xl w-[45%]  bg-blue-400 py-6 px-9'>
+      <h2 className='text-3xl font-semibold'>0</h2>
+      <h3 className='text-xl font-medium'>New Task</h3>
+      
+      </div>
+      <div className='rounded-xl w-[45%]  bg-green-400 py-6 px-9'>
+      <h2 className='text-3xl font-semibold'>0</h2>
+      <h3 className='text-xl font-medium'>New Task</h3>
+      
+      </div>
+      <div className='rounded-xl w-[45%]  bg-yellow-400 py-6 px-9'>
+      <h2 className='text-3xl font-semibold'>0</h2>
+      <h3 className='text-xl font-medium'>New Task</h3>
+      
+      </div>
+        
       
     </div>
   )
