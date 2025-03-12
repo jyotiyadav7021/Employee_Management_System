dfaf2d7b266f6f5ff748642cc1ef4ab0c009a709
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-12 23:58:40 +0530

    new

--- emc/src/components/Other/AllTask.jsx
+++ emc/src/components/Other/AllTask.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 const AllTask = () => {
   return (
-    <div className='bg-[1c1c1c] p-5 rounded mt-5'>
-        <div className='bg-red-400 py-2 px-4 flex justify-between rounded'>
+    <div className='bg-[1c1c1c] p-5 rounded mt-5 h-48 overflow-auto'>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Jyoti</h2>
+            <h3>Make a ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Jyoti</h2>
+            <h3>Make a ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Jyoti</h2>
+            <h3>Make a ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Jyoti</h2>
+            <h3>Make a ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2>Jyoti</h2>
+            <h3>Make a ui design</h3>
+            <h5>Status</h5>
+        </div>
+        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2>Jyoti</h2>
             <h3>Make a ui design</h3>
             <h5>Status</h5>
