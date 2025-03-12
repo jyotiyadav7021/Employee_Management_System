693efc1fdd95eb3cfd0455b3c1e8c7aa27cfb35b
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-12 23:23:45 +0530

    new

--- emc/src/components/Dashboard/AdminDashboard.jsx
+++ emc/src/components/Dashboard/AdminDashboard.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 import Header from '../Other/Header'
+import CreateTask from '../Other/CreateTask'
+import AllTask from '../Other/AllTask'
 
 const AdminDashboard = () => {
   return (
-    <div>
+    <div className='h-screen w-full p-7'>
       <Header/>
+      <CreateTask/>
+      <AllTask/>
+
+      
     </div>
   )
 }
