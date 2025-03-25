eaeb35b8953740a336d26054da7b5931f681213a
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-25 23:58:47 +0530

    new

--- emc/src/components/TaskList/CompleteTask.jsx
+++ emc/src/components/TaskList/CompleteTask.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CompleteTask = () => {
   return (
     <div>
-      <h1>real task </h1>
+      
     </div>
   )
 }
--- emc/src/components/TaskList/FailedTask.jsx
+++ emc/src/components/TaskList/FailedTask.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FailedTask = () => {
   return (
     <div>
-      <h1>real task</h1>
+      
     </div>
   )
 }
