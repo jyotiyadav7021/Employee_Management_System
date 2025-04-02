407275b239752cc8c5fbe2d1ed94fea435c225a6
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-04-03 00:17:04 +0530

    new

--- emc/src/components/TaskList/NewTask.jsx
+++ emc/src/components/TaskList/NewTask.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 
-const NewTask = () => {
+const NewTask = ({data}) => {
   return (
     <div>
       <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-300 rounded-xl'>
         <div className='flex justify-between items-center'>
-            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
             <h4 className='text-sm'>20 feb 2024</h4>
 
         </div>
-        <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+        <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
         <p className='text-sm mt-2'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores voluptas dolore, aut rem accusantium ea in corporis molestias repellendus, repudiandae, impedit dolor optio iusto corrupti enim quia magni quaerat. Earum.
+            {data.description}
         </p>
         </div>
         <div>
